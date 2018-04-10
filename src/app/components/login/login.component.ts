bc8c1e4f4ca1d2ad0820899cc5eba17ad6762161
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-04-10 19:40:30 +0200

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
 import { User } from '../../user';
 
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -13,7 +15,10 @@ export class LoginComponent implements OnInit {
 
   userl: any = {};
 
-  constructor(private userservice: UserService) { }
+  constructor(private userservice: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
@@ -22,7 +27,15 @@ export class LoginComponent implements OnInit {
   login(): void{
     
     this.userservice.loginUser(this.userl)
-    .subscribe(user => this.userl = user);
+    .subscribe(user => { this.userl = user;
+             this.router.navigate(['/dashboard']);
+              },
+              error => {
+
+                console.log("neispravna lozinka");
+              }
+          
+          );
 
   }
 

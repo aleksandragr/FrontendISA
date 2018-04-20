1686662c0e5a768d6e7f213a646898463328a604
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-04-20 03:34:51 +0200

    aaaaaa

--- src/app/components/scale/scale.component.ts
+++ src/app/components/scale/scale.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminsystemService } from '../../services/adminsystem/adminsystem.service';
 import { scale } from '../../scale';
+import { UserService } from '../../services/user/user.service';
+import { LoggedinService } from '../../services/loggedin/loggedin.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-scale',
@@ -10,11 +13,16 @@ import { scale } from '../../scale';
 export class ScaleComponent implements OnInit {
 
   newScale: any={};
-
+  u: User;
   scale: scale;
-  constructor(private adminSystemService: AdminsystemService) { }
+  constructor(private adminSystemService: AdminsystemService,
+        private loggedin: LoggedinService
+  ) { }
 
   ngOnInit() {
+    var ua = this.loggedin.getLocalStore();
+    
+    this.u = ua;
   }
 
   addscalee(): void{
